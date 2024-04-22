8752f8892348687dc17f9d67b2012bb874069f6a
liamRouaz <liam.rouaz@laplateforme.io>
2024-04-22 13:24:10 +0200

    ajout de l'affichage des film avec leur affiche

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ const api = 'https://api.themoviedb.org/3/discover/movie?api_key=' + maConst;
 fetch(api)
   .then(response => response.json())
   .then(data => {
-    // Faire quelque chose avec les données récupérées
-    console.log(data);
+    const filmsContainer = document.getElementById('films');
+
+    data.results.forEach(film => {
+        const filmElement = document.createElement('div');
+        filmElement.innerHTML = `
+            <h2>${film.title}</h2>
+            <img src="https://image.tmdb.org/t/p/w500/${film.poster_path}" alt="${film.title} Poster">
+            <p>${film.overview}</p>
+        `;
+        filmsContainer.appendChild(filmElement);
+    });
   })
   .catch(error => {
     console.error('Erreur lors de la récupération des données:', error);
